31a710ddf7975f5965db119392c22f095bb2d14a
Vasiliy Spassky <spassky99@gmail.com>
2023-12-11 12:44:34 +0200

    Use `logging` instead of `print` in the example

--- examples/interaction_workflow.py
+++ examples/interaction_workflow.py
@@ -1,9 +1,13 @@
 """A script demonstrating the interaction workflow with the Hunter.io API bindings provided by the library."""
 
+import logging
+
 from hunter_client.client import HunterClient
 from hunter_client.exceptions import HunterError, InvalidInputError, TooManyRequestsError
 from hunter_client.storages.dummy import DummyStorage
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 hunter_client = HunterClient(api_key='qwerty12345')
 emails_storage = DummyStorage[str, list[str]]()
 
@@ -13,17 +17,17 @@ for domain in domains_to_search:
     try:
         emails = hunter_client.search_emails_by_domain(domain)
     except InvalidInputError:
-        print('Invalid input for domain: {0}'.format(domain))
+        logger.info('Invalid input for domain: {0}'.format(domain))
     except TooManyRequestsError:
-        print('Too many requests. Please try again later.')
+        logger.error('Too many requests. Please try again later.')
     except HunterError:
-        print('An unknown error occurred.')
+        logger.error('An unknown error occurred.')
     else:
-        print('Emails found for {0}: {1}'.format(domain, emails))
+        logger.info('Emails found for {0}: {1}'.format(domain, emails))
         emails_storage.set(domain, emails)
 
     stored_emails = emails_storage.get(domain)
     if stored_emails:
-        print('Stored emails for {0}: {1}'.format(domain, stored_emails))
+        logger.info('Stored emails for {0}: {1}'.format(domain, stored_emails))
     else:
-        print('No emails stored for {0}'.format(domain))
+        logger.info('No emails stored for {0}'.format(domain))
