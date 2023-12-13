a17a74152bbe34cc6e7d5f6700fea20507126086
Vasiliy Spassky <spassky99@gmail.com>
2023-12-13 02:10:06 +0200

    Minor style change

--- tests/test_client_raises_exceptions.py
+++ tests/test_client_raises_exceptions.py
@@ -12,7 +12,6 @@ def test_dispatch_invalid_input_exception() -> None:
             'https://api.hunter.io/v2/domain-search?domain=invalid_domain',
             status_code=requests.codes.bad_request,
         )
-
         client = HunterClient(api_key='not_really_an_api_key')
         with pytest.raises(InvalidInputError):
             client.search_emails_by_domain('invalid_domain')
@@ -24,7 +23,6 @@ def test_dispatch_too_many_requests_exception() -> None:
             'https://api.hunter.io/v2/domain-search?domain=example.com',
             status_code=requests.codes.too_many_requests,
         )
-
         client = HunterClient(api_key='not_really_an_api_key')
         with pytest.raises(TooManyRequestsError):
             client.search_emails_by_domain('example.com')
@@ -36,7 +34,6 @@ def test_dispatch_server_error_exception() -> None:
             'https://api.hunter.io/v2/domain-search?domain=example.com',
             status_code=requests.codes.internal_server_error,
         )
-
         client = HunterClient(api_key='not_really_an_api_key')
         with pytest.raises(HunterServerError):
             client.search_emails_by_domain('example.com')
@@ -48,7 +45,6 @@ def test_dispatch_generic_client_error_exception() -> None:
             'https://api.hunter.io/v2/domain-search?domain=example.com',
             status_code=requests.codes.im_a_teapot,
         )
-
         client = HunterClient(api_key='not_really_an_api_key')
         with pytest.raises(HunterError):
             client.search_emails_by_domain('example.com')
