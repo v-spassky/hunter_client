84c69d8d2f849325dbe5d465ddba45eb17057852
Vasiliy Spassky <spassky99@gmail.com>
2023-12-13 02:11:09 +0200

    Remove more unnecessary `pass`es

--- hunter_client/exceptions.py
+++ hunter_client/exceptions.py
@@ -4,22 +4,14 @@
 class HunterError(Exception):
     """Base class for all exceptions raised by the `hunter_client` library."""
 
-    pass  # noqa: WPS420, WPS604
-
 
 class TooManyRequestsError(HunterError):
     """Exception raised when the Hunter API returns a 429 response."""
-
-    pass  # noqa: WPS420, WPS604
 
 
 class InvalidInputError(HunterError):
     """Exception raised when the Hunter API returns a 400 response."""
 
-    pass  # noqa: WPS420, WPS604
-
 
 class HunterServerError(HunterError):
     """Exception raised when the Hunter API returns a 500 response."""
-
-    pass  # noqa: WPS420, WPS604
